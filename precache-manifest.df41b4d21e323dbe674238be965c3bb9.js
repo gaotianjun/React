588b6a595fec7e8f03fbc26b3182371b6004011f
gaotianjun <214938630@qq.com>
2020-10-04 12:28:08 +0800

    react

---
+++ precache-manifest.df41b4d21e323dbe674238be965c3bb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcbf11283e1b3e88dffbaabfa9f2055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5f14794200672cad892",
+    "url": "/static/js/2.476b2e68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.476b2e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9959b1864ff55cccac28",
+    "url": "/static/js/main.299d5354.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
